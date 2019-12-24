e886df78dda91950a0a04d24013b241cad4e44c3
Natalie Groeneboom <nataliegroeneboom@users.noreply.github.com>
2019-12-24 10:56:36 +0000

    add all files for react native practice project

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import {View, Text, StyleSheet, FlatList, Button, TouchableOpacity} from 'react-native';
+import { Form, TextValidator } from 'react-native-validator-form';
+import {Feather} from '@expo/vector-icons';
+
+
+class IndexScreen extends React.Component{
+   
+    state = {
+        email: '',
+    }
+    
+
+    handleChange = (email) => {
+        setState({ email });
+    }
+    render(){
+
+
+        return (
+  
+            <View>
+                    <FlatList 
+                        data={state}
+                        keyExtractor={(blogPost) => blogPost.title}
+                        renderItem={({item}) =>{
+                        return (<TouchableOpacity 
+                            onPress={() => navigation.navigate('Show', {id: item.id})}>
+                                <View style={styles.row}>
+                                <Text style={styles.title}>{item.title} - {item.id}</Text>
+                                <TouchableOpacity onPress={
+                                        () => deleteBlogPost(item.id)
+                                        }>
+                                        <Feather style={styles.icon} name="trash" />
+                                </TouchableOpacity>
+                                </View>
+                            </TouchableOpacity>)
+                        } }
+                        />
+                          <Form
+                        ref="form"
+                        onSubmit={this.handleSubmit}
+                    >
+                        <TextValidator
+                            name="email"
+                            label="email"
+                            validators={['required', 'isEmail']}
+                            errorMessages={['This field is required', 'Email invalid']}
+                            placeholder="Your email"
+                            type="text"
+                            keyboardType="email-address"
+                            value={email}
+                            onChangeText={this.handleChange}
+                        />
+                         <Button
+                            title="Submit"
+                            onPress={this.handleSubmit}
+                        />
+                    </Form>
+            </View>
+          )
+    }
+  
+}
+
+IndexScreen.navigationOptions = ({navigation}) => {
+    return {
+        headerRight: <TouchableOpacity 
+        onPress= {() => navigation.navigate('Create')}>
+                        <Feather name="plus" size={30} />
+                    </TouchableOpacity>
+    };
+}
+
+const styles = StyleSheet.create({
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 20,
+        paddingHorizontal: 10,
+        borderTopWidth: 1,
+        borderColor: 'gray'
+    },
+    title: {
+        fontSize: 18
+    },
+    icon: {
+        fontSize: 24
+    }
+});
+
+
+export default IndexScreen;
